95e8d6a902fc8a43f57dbde11f36b69b9c0c28d3
xt-t <xt.trinh@gmx.de>
2022-01-17 21:08:16 +0100

    correcting error in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,48 +2,8 @@ import './App.scss';
 import MainPage from "./pages/MainPage";
 import {Route, Routes} from "react-router-dom";
 import SecPage from "./pages/SecPage";
-import {
-    addShoppingitem, deleteShoppingitem, deleteWholeList,
-    getAllShoppingitems, getShoppingitemById, updateShoppingitem
-} from "./service/shoppinglist-api-service";
-import {Shoppingitem} from "./components/Shoppingitem";
-import {useEffect, useState} from "react";
 
 export default function App() {
-
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        getAllShoppingitems()
-            .then(items => setProducts(items))
-            .catch(error => console.error(error))
-    }, [])
-
-    const addItem = (product:Shoppingitem) => {
-        addShoppingitem(product.id)
-            .then(() => getAllShoppingitems())
-            .then(products => setProducts(products))
-            .catch(error => console.error(error))
-    }
-
-    const deleteShoppinglist = () => {
-        deleteWholeList()
-    }
-
-    const getItemById = (id:String) => {
-        getShoppingitemById(id)}
-
-    const deleteItem  = (id:String) =>
-        deleteShoppingitem(id)
-            .then(() => getAllShoppingitems())
-            .then(items => setProducts(items))
-            .catch(error => console.error(error))
-
-    const updateItem  = (product:Shoppingitem) =>
-        updateShoppingitem(product)
-            .then(() => getAllShoppingitems())
-            .then(products => setProducts(products))
-            .catch(error => console.error(error))
 
         return (
         <div className="App">
