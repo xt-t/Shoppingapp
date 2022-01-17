1886523c519b8e19eb31d00b245e46d9765d33f5
xt-t <xt.trinh@gmx.de>
2022-01-17 22:10:02 +0100

    correction product values api

--- frontend/src/service/shoppinglist-api-service.ts
+++ frontend/src/service/shoppinglist-api-service.ts
@@ -8,17 +8,17 @@ interface Shoppingitem {
 }
 
 export const getAllShoppingitems = () =>
-    axios.get('/api/shop').then(response => response.data)
+    axios.get(`/api/shop`).then(response => response.data)
 
-export const addShoppingitem = (item:Shoppingitem) => axios.post('/api/shop/', item)
+export const addShoppingitem = (product:Shoppingitem) => axios.post(`/api/shop/`, product)
 
-export const deleteWholeList = () => axios.delete('/api/shop/')
+export const deleteWholeList = () => axios.delete(`/api/shop/`)
 
 export const getShoppingitemById = (id: String) =>
-    axios.get('/api/shop/${id}').then(response => response.data)
+    axios.get(`/api/shop/${id}`).then(response => response.data)
 
-export const deleteShoppingitem = (id:String) => axios.delete('/api/shop/${id}')
+export const deleteShoppingitem = (id:String) => axios.delete(`/api/shop/${id}`)
 
 export const updateShoppingitem = (product:Shoppingitem) =>
-    axios.put('/api/shop/${product.id}/update', product)
+    axios.put(`/api/shop/${product.id}/update`, product)
 
